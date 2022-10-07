17b4b5c03cb29f18ce933988f9ae82748f7f1224
opuzayed <zayedopu4933@gmail.com>
2022-10-07 19:22:26 +0600

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layouts/Main';
+import Shop from './components/Shop/Shop';
+import About from './components/About/About';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   const router = createBrowserRouter([
     { 
        path:'/',
-       element:<Main></Main>
+       element:<Main></Main>,
+       children:[
+        {
+        path:'/',
+        element:<Shop></Shop>
+        },
+        {
+          path:'orders',
+          element:<Orders></Orders>
+        },
+        {
+          path:'inventory',
+          element:<Inventory></Inventory>
+        },
+        {
+          path:'about',
+          element:<About></About>
+        }
+       ]
     }
+    
     ]);
   return (
     <div>
